2a36789756d6dee696fb70dc7a9199d9eb1c94a5
Tomislav Bosnić <bosnict@yahoo.com>
2016-03-16 15:23:14 +0100

    New

--- aplikacija/aplikacija/Program.cs
+++ aplikacija/aplikacija/Program.cs
@@ -52,7 +52,6 @@ public class Validation
 
         if (string.IsNullOrEmpty(val1))
         {
-            Console.WriteLine("You need to insert value.");
             return "null";
         }
         return val1;
@@ -69,8 +68,7 @@ public class Validation
         else
         {
             if (!float.TryParse(gp, out f_value))
-            {
-                Console.WriteLine("You need to insert numerical value.");
+            { 
                 return 0;
             }
             else
@@ -86,74 +84,83 @@ public class Validation
         static void Main()
         {
             List<Student> students = new List<Student>(2);
-
-        start:
-            Validation v = new Validation();
-            Console.WriteLine("Operation:");
-            string op = Console.ReadLine();
-
-            op = op.ToUpper();
-
-            if (string.Equals(op, Operation.Enlist))
+            int temp=0;
+            do
             {
-                Console.WriteLine("Student");
+                Validation v = new Validation();
+                Console.WriteLine("Operation:");
+                string op = Console.ReadLine();
+                op = op.ToUpper();
+                bool check_enlist = op.Equals(Operation.Enlist);
+                bool check_display = op.Equals(Operation.Display);
 
-
-            fn_goto:
-
-                Console.WriteLine("First name: ");
-                string fn = Console.ReadLine();
-                if (v.Val_String(fn) == "null")
+                while (check_enlist == false && check_display == false) 
                 {
-                    goto fn_goto;
+                    Console.WriteLine("Operation non-existing, please use appropriate operation");
+                    Console.WriteLine("Operation:");
+                    op = Console.ReadLine();
+                    op = op.ToUpper();
+                    check_enlist = op.Equals(Operation.Enlist);
+                    check_display = op.Equals(Operation.Display);
+                } 
+
+
+
+                if (string.Equals(op, Operation.Enlist))
+                {
+                    Console.WriteLine("Student");
+
+                    Console.WriteLine("First name: ");
+                    string fn = Console.ReadLine();
+                    while (v.Val_String(fn) == "null")
+                    {
+                        Console.WriteLine("You need to insert value.");
+                        Console.WriteLine("First Name:");
+                        fn = Console.ReadLine();
+                    }
+
+                    Console.WriteLine("Last name: ");
+                    string ln = Console.ReadLine();
+                    while (v.Val_String(ln) == "null")
+                    {
+                        Console.WriteLine("You need to insert value.");
+                        Console.WriteLine("Last name:");
+                        ln = Console.ReadLine();
+                    }
+
+                    Console.WriteLine("GPA: ");
+                    string gpa = Console.ReadLine();
+                    while (v.Val_GPA_Type(gpa) == 0)
+                    {
+                        Console.WriteLine("You need to insert numerical value.");
+                        Console.WriteLine("GPA: ");
+                        gpa = Console.ReadLine();
+                    }
+
+                    string id = StudentIdGenerator.IdCreate();
+
+                    Student s = new Student(id, fn, ln, v.Val_GPA_Type(gpa));
+
+                    students.Add(s);
                 }
-
-
-            ln_goto:
-
-                Console.WriteLine("Last name: ");
-                string ln = Console.ReadLine();
-                if (v.Val_String(ln) == "null")
+                else if (string.Equals(op, Operation.Display))
                 {
-                    goto ln_goto;
+                    int a = 1;
+                    students.Sort(delegate (Student x, Student y)
+                    {
+                        return x._LastName.CompareTo(y._LastName);
+                    });
+                    foreach (Student s in students)
+                    {
+
+                        Console.WriteLine("{0}. {1}, {2} - {3}", a++, s._LastName, s._FirstName, (float)s._GPA);
+
+                    }
+
+                    Console.ReadKey();
+                    temp = 1;
                 }
-
-            gpa_goto:
-                Console.WriteLine("GPA: ");
-                string gpa = Console.ReadLine();
-                if (v.Val_GPA_Type(gpa) == 0)
-                {
-                    goto gpa_goto;
-                }
-
-                string id = StudentIdGenerator.IdCreate();
-
-                Student s = new Student(id, fn, ln, v.Val_GPA_Type(gpa));
-
-                students.Add(s);
-                goto start;
-            }
-            else if (string.Equals(op, Operation.Display))
-            {
-                int a = 1;
-                students.Sort(delegate (Student x, Student y)
-                {
-                    return x._LastName.CompareTo(y._LastName);
-                });
-                foreach (Student s in students)
-                {
-
-                    Console.WriteLine("{0}. {1}, {2} - {3}", a++, s._LastName, s._FirstName, (float)s._GPA);
-
-                }
-
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("Operation non-existing, please use appropriate operation");
-                goto start;
-            }
+            } while (temp == 0);
         }
     }
 }
